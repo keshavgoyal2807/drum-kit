0504d5713b14e8963b81bafa819441227adc68c5
KESHAV GOYAL <keshavgoyal2014@gmail.com>
2018-10-28 11:01:27 +0530

    added files

---
+++ script.js
@@ -0,0 +1,19 @@
+window.addEventListener('keydown',function(e){
+  var a= document.querySelector(`audio[data-first="${e.keyCode}"]`);
+  var key=document.querySelector(`.first[data-first="${e.keyCode}"]`);
+  if(!a)
+  {
+    return;//stop if another key is clicked
+  }
+  a.currentTime=0;    //rewind to start
+  a.play();
+  key.classList.add('playing');
+  var key1=document.querySelectorAll('.first');
+  key1.forEach(key2 => key2.addEventListener('transitionend',function(e){
+    if(e.propertyName!=='transform')
+    {
+      return;
+    }
+    this.classList.remove('playing');
+  }));
+});
